e56d1553cf258351ea84f3ffea0439af1834d68e
Orangehawk <48203578+Orangehawk@users.noreply.github.com>
2022-04-27 17:01:17 +0800

    Basic Table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.dark.css';
+import { Table, Tag, Space } from 'antd';
+
+
+const SortScores = (data) => {
+  data.sort((a, b) => { return b[1] - a[1]; });
+  return data;
+}
+
+const MakePlayerEntry = (place, name, score) => {
+    return {place: place, name: name, paniks: score};
+}
+
+const Leaderboard = () => {
+
+    //Place	Name Paniks
+    const columns = [
+        {
+            title: "Place",
+            dataIndex: "place",
+        },
+        {
+            title: "Name",
+            dataIndex: "name",
+        },
+        {
+            title: "Paniks",
+            dataIndex: "paniks",
+        }
+    ];
+    const data = [];
+    const scores = [["Alcara", 7], ["Av'yana", 45], ["Ayleth", 9], ["Crimson", 7], ["Ki'sae", 30], ["Miniya", 3], ["Mitsue", 5], ["Otaku", 4], ["R'aeyon", 11], ["Reina", 12], ["Reshina", 10], ["Rien", 11], ["Rorik", 2], ["Shiro", 6], ["Yuza", 2], ["Al", 1], ["Anna", 5], ["Agnes", 1], ["Banana", 3], ["Renlino", 2], ["Chungwoo", 1]];
+
+    SortScores(scores);
+
+    for(var i = 0; i < scores.length; i++) {
+        let place = 0;
+
+        if(i>1 && scores[i][1] == scores[i-1][1])
+            place = data[i - 1].place;
+        else
+            place = i + 1;
+
+        data.push(MakePlayerEntry(place, scores[i][0], scores[i][1]));
+    }
+
+    console.log(scores.length);
+    console.log(data);
+
+    return <Table columns={columns} dataSource={data} pagination={false} />;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Leaderboard />;
 }
 
 export default App;
