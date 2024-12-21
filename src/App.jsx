e28164db3f122477c5849170a6c10d6a03ab8e9e
Orangehawk <48203578+Orangehawk@users.noreply.github.com>
2024-12-21 12:59:26 +0800

    Working on v2 quick commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Card, Chip, createTheme, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, ThemeProvider, Typography } from '@mui/material'
+import { dark } from '@mui/material/styles/createPalette'
+import { getPlayersFromDatabase } from './firebaseHelper'
+import moment from 'moment'
 
-function App() {
+function TestDisplay() {
   const [count, setCount] = useState(0)
 
   return (
@@ -18,9 +22,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -30,6 +34,70 @@ function App() {
       </p>
     </>
   )
+}
+
+function Leaderboard() {
+
+  const fakeData = [
+    {"name":"A", "score":Math.floor(Math.random() * 20)},
+    {"name":"B", "score":Math.floor(Math.random() * 20)},
+    {"name":"C", "score":Math.floor(Math.random() * 20)},
+    {"name":"D", "score":Math.floor(Math.random() * 20)},
+    {"name":"E", "score":Math.floor(Math.random() * 20)},
+    {"name":"F", "score":Math.floor(Math.random() * 20)},
+    {"name":"G", "score":Math.floor(Math.random() * 20)},
+  ]
+
+  fakeData.sort((a, b) => b.score - a.score)
+
+  return (
+    <Stack>
+      <Card variant="outlined">
+      <Typography variant="h3" gutterBottom>
+        Panik Leaderboard
+      </Typography>
+      <TableContainer component={Card}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Place</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Paniks</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              fakeData.map((row, i) =>(
+                <TableRow key={row.name}>
+                <TableCell>{i+1}</TableCell>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.score}</TableCell>
+              </TableRow>
+              ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+      </Card>
+    </Stack>
+  )
+}
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  }
+})
+
+async function App()  {
+  var date = moment().subtract(5, "days")
+  var result = await getPlayersFromDatabase(date)
+  console.log(result)
+  return (
+  <ThemeProvider theme={darkTheme}>
+    <Leaderboard></Leaderboard>
+  </ThemeProvider>
+)
 }
 
 export default App
